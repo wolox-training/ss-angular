8f19bafa6425f4506d66714c8e36e26433fb8dfc
Sara Santacruz <sara.santacruz@wolox.co>
2019-10-08 11:50:54 -0500

    changes fixeds

--- app/app.js
+++ app/app.js
@@ -8,21 +8,15 @@ bookscatApp.config($stateProvider => {
   .state('registerState',{
     url: '/sign-up',
     component: 'register'
-  });
-
-  $stateProvider
+  })
   .state('loginState',{
     url: '/login',
     component: 'login'
-  });
-
-  $stateProvider
+  })
   .state('booksState',{
     url: '/',
     component: 'books'
-  });
-
-  $stateProvider
+  })
   .state('detailState',{
     url: '/books/{bookId}',
     component: 'detail'
